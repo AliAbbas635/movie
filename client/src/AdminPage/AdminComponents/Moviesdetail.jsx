c43da54e46d3f3675cfa70d24b8cd7bae38455c4
ALINouman777 <129924156+ALINouman777@users.noreply.github.com>
2023-12-08 12:19:43 -0700

    fix table

--- client/src/AdminPage/AdminComponents/Moviesdetail.jsx
+++ client/src/AdminPage/AdminComponents/Moviesdetail.jsx
@@ -5,81 +5,83 @@ import "./User.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-
 const Moviesdetail = () => {
-  const { AllMovie, allmovie,user } = useContext(MyContext);
+  const { AllMovie, allmovie, user } = useContext(MyContext);
   useEffect(() => {
     AllMovie();
-  
   }, []);
 
   function truncateDescription(description) {
-    const words = description.split(' ');
-    const truncated = words.slice(0, 25).join(' ');
-  
+    const words = description.split(" ");
+    const truncated = words.slice(0, 25).join(" ");
+
     if (words.length > 25) {
       return `${truncated}...`;
     }
-  
+
     return truncated;
   }
-  
 
   async function onDelete(id) {
-    const response = await axios
-      .delete(`http://localhost:5000/user/${id}`, {
+    await axios
+      .delete(`http://localhost:5000/movie/${id}`, {
         withCredentials: true,
       })
       .then((res) => {
-        console.log("deleted Successfully")
+        console.log("deleted Successfully");
+        AllMovie();
       })
       .catch((err) => {
         console.log(err);
       });
   }
 
-  console.log(allmovie[0])
-
   return (
     <>
-      {user && user.MyProfile && user.MyProfile.isAdmin ?  
-      <>
-      <h1 style={{ margin: "1rem" }}>All Movies List</h1>
-      <div>
-        <Link className="dashboardbtn" to={"/dashboard"}>Dashboard</Link>
-      </div>
-
-      <div>
-        <Link className="dashboardbtn btnleft" to={"/upload"}>Add New Movie</Link>
-      </div>
-      
-      <table className="movie-table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Genre</th>
-            <th>Rating</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {allmovie.map((movie, index) => (
-            <tr key={index}>
-              <td>{movie.title}</td>
-              <td>{truncateDescription(movie.desc)}</td>
-              <td>{movie.gener}</td>
-              <td>{movie.limit}</td>
-              {console.log(movie._id)}
-              <td>
-                <button onClick={() => onDelete(movie._id)}>Delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      </>
-      : <h1>You are not Allowed</h1>}
+      {user && user.MyProfile && user.MyProfile.isAdmin ? (
+        <>
+          <h1 style={{ margin: "1rem" }}>All Movies List</h1>
+          <div>
+            <Link className="dashboardbtn" to={"/dashboard"}>
+              Dashboard
+            </Link>
+          </div>
+          <div>
+            <Link className="dashboardbtn btnleft" to={"/upload"}>
+              Add New Movie
+            </Link>
+          </div>
+          <table className="movie-table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Genre</th>
+                <th>Rating</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {allmovie &&
+                allmovie.map((movie, index) => (
+                  <tr key={index}>
+                    <td>{movie.title}</td>
+                    <td>{truncateDescription(movie.desc)}</td>
+                    <td>{movie.gener}</td>
+                    <td>{movie.limit}</td>
+                    <td>
+                      <button onClick={() => onDelete(movie._id)}>
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </>
+      ) : (
+        <h1>You are not Allowed</h1>
+      )}
     </>
   );
 };
