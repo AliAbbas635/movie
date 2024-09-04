2b65926c1e6aec7c0cee06a80b686a9ab825ad02
AliAbbas635 <alijafry014@gmail.com>
2024-09-04 05:58:55 -0700

    fixing url

--- client/src/BaseURL.js
+++ client/src/BaseURL.js
@@ -1,7 +1,7 @@
 // const BaseURL = "https://mern-web-app.azurewebsites.net";
 
-// const BaseURL="http://3.84.53.73:5001"
+const BaseURL="http://3.84.53.73:5001"
 
-const BaseURL= "http://localhost:5001" 
+// const BaseURL= "http://localhost:5001" 
 
 export default BaseURL;
