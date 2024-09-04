a45478886e4dc075904202c510d60edcabee960b
AliAbbas635 <alijafry014@gmail.com>
2024-09-04 14:19:02 -0700

    fixing the backend and frontend

--- client/src/BaseURL.js
+++ client/src/BaseURL.js
@@ -1,6 +1,5 @@
-// const BaseURL = "https://mern-web-app.azurewebsites.net";
 
-const BaseURL="http://3.84.53.73:5001"
+const BaseURL="http://35.171.7.29:5001"
 
 // const BaseURL= "http://localhost:5001" 
 
